afa9cfdcc6ba6752f59a3d13674e4e6d9473eee8
lea arcidiacono <leaarcidiacono@leas-MBP.fios-router.home>
2023-11-30 22:02:04 -0500

    special character choices

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
 // Assignment code here
 var answer = window.confirm("Do you want to create a password? Please click okay to begin.");
 
-
-if (answer) {
-    // User clicked "ok"
-    console.log("User wants to proceed.");
-} else {
-    // User clicked "no"
-    console.log("User canceled.");
-}
 // Prompts user to enter a password length
 var passwordLength = prompt("Enter the length of password at least 8 characters but no more than 128 characters. ")
-
 
 //Prompts user to enter lowercase, uppercase, and/or special characters
 var includeLowerCase = confirm("Include lowercase letters? Click OK for yes, or CANCEL for no");
@@ -19,12 +10,15 @@ var includeUpperCase = confirm("Include uppercase letters? Click OK for yes, or 
 var includeNumbers = confirm("Include numbers? Click OK for yes, or CANCEL for no");
 var includesSpecialCharacters = confirm("Include special characters? Click OK for yes, or CANCEL for no");
 
+var upperCaseLetters = ' A B C D E F G H I J K L M N O P Q R S T U V W X Y Z';
+var lowerCaseLetters = 'a b c d e f g h i j k l m n o p q r s t u v w x y z';
+var potentialCharacters = [];
+var numbers = 123456789;
+var specialCharacters = '! @ # $ * &';
 
-console.log("Include lowercase letters:", includeLowerCase);
-console.log("Include uppercase letters:", includeUpperCase);
-console.log("Include numbers:", includeNumbers);
-console.log("Include special characters:", includesSpecialCharacters);
-
+if(upperCase) {
+  
+};
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
